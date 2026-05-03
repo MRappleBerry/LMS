27590bb43ba47cf55484a2c2b2a42809a9b1db4f
Jonathan <jonathan@lexisai.dev>
2026-05-03 09:23:10 -0700

    fix: bump cache key to v2 to clear stale empty-subject cache

--- lms-app/client/src/lib/curriculumApi.js
+++ lms-app/client/src/lib/curriculumApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_BASE = import.meta.env.VITE_API_URL || ''
-const CACHE_KEY = 'lexisai.curriculum.cache.v1'
+const CACHE_KEY = 'lexisai.curriculum.cache.v2'
 const USER_KEY = 'lexisai.user.id'
 const mem = new Map()
 
